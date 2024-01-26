ed369234e3cc877971c5413022dbe3ddf6d013d9
Seb <lecampus@users.noreply.github.com>
2024-01-26 15:52:38 +0100

    implement isEnd()

--- src/fr/CDA25/SebIsma/games/tictactoe/TicTacToe.java
+++ src/fr/CDA25/SebIsma/games/tictactoe/TicTacToe.java
@@ -9,7 +9,16 @@ public class TicTacToe extends Board {
     }
 
     @Override
-    protected boolean isEnd() {
-        return true;
+    protected boolean isEnd(int[] last_move) {
+        boolean a = checkDir(last_move, new int[]{1, 1}, 3);
+        boolean b = checkDir(last_move, new int[]{1, 0}, 3);
+        boolean c = checkDir(last_move, new int[]{0, 1}, 3);
+        boolean d = checkDir(last_move, new int[]{1, -1}, 3);
+
+        if(a || b || c || d) {
+            winner = board[last_move[1]][last_move[0]].getOwner();
+            return true;
+        } else
+            return checkFull();
     }
 }
