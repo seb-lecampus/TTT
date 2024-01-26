a58589e45d097bc9a00380df69173c845d0a0341
ismael vas <155621485+isma444@users.noreply.github.com>
2024-01-26 10:49:06 +0100

    extends board and implement

--- src/fr/CDA25/SebIsma/games/puissance4/Puissance4.java
+++ src/fr/CDA25/SebIsma/games/puissance4/Puissance4.java
@@ -1,4 +1,10 @@
 package fr.CDA25.SebIsma.games.puissance4;
 
-public class Puissance4 {
+import fr.CDA25.SebIsma.games.boad.Board;
+
+public class Puissance4 extends Board {
+    @Override
+    protected boolean isEnd() {
+        return false;
+    }
 }
